a012c20c867658d3085aaf6511c291145f35dc35
Nourreddine1920 <nourreddine2000@gmail.com>
2022-10-24 10:33:12 +0100

    adding overloading example

---
+++ c++ inheritance/overloading.cpp
@@ -0,0 +1,40 @@
+/***********************
+You can have multiple definitions for the same function name in the same scope. 
+The definition of the function must differ from each other by the types and/or 
+the number of arguments in the argument list. 
+You cannot overload function declarations that differ only by return type.
+****************************/
+
+
+
+
+#include <iostream>
+using namespace std;
+ 
+class printData {
+   public:
+      void print(int i) {
+        cout << "Printing int: " << i << endl;
+      }
+      void print(double  f) {
+        cout << "Printing float: " << f << endl;
+      }
+      void print(char* c) {
+        cout << "Printing character: " << c << endl;
+      }
+};
+
+int main(void) {
+   printData pd;
+ 
+   // Call print to print integer
+   pd.print(5);
+   
+   // Call print to print float
+   pd.print(500.263);
+   
+   // Call print to print character
+   pd.print("Hello C++");
+ 
+   return 0;
+}
